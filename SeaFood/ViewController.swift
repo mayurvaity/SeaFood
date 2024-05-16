89bc14577c028797c1bc921c913d9a3558bcd4e7
Mayur Vaity <mayur.m.vaity@icloud.com>
2024-05-17 00:52:13 +0530

    Initial Commit

---
+++ SeaFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeaFood
+//
+//  Created by Mayur Vaity on 17/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
